fe10553c5c9a1e69fc5e997a1158b0999aef7404
subhadipghs <ghoshsubhadipghosh@gmail.com>
2020-08-05 11:03:22 +0530

    First Commit

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,27 @@
-import Nav from '../components/nav'
+import Layout from '../components/Layout/Layout';
+import Card from '../components/Card/Card';
+
+const cardContents = [
+	{ title: "Get 🍕 for free", contents: "Call Dominos and tell them the coupon 184fw2. They will serve you with a free pizza. 🎉 Celebrate", 
+		tags: ["Pizza", "Free", "Dominos", "Blog"]
+	}
+]
+
 
 export default function IndexPage() {
   return (
-    <div>
-      <Nav />
-      <div className="py-20">
-        <h1 className="text-5xl text-center text-accent-1">
-          Next.js + Tailwind CSS
-        </h1>
+    <Layout>
+      <div className="flex">
+      	{cardContents.map(({ title, contents, tags }) => {
+      		return (
+      			<Card
+      				title={title}
+      				contents={contents}
+      				tags={tags}
+      			/>
+      		);
+      	})}
       </div>
-    </div>
+    </Layout>
   )
 }
